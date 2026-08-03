780f71875e2760d5f1258b09e9832e59c2b664a9
jonber2185 <jonber2185@gmail.com>
2026-08-03 22:05:26 +0900

    feat: Add options for Code Update

--- commands/code/update_code.py
+++ commands/code/update_code.py
@@ -9,15 +9,19 @@ from .. import utils
     code="코드",
     map_name="맵",
     difficulty="난이도",
-    description="설명",
     creator="제작자",
+    cp="단계",
+    description="설명",
+    guide="가이드"
 )
 @app_commands.describe(
     code="기존 코드 (5자리)",
     map_name="새로운 맵 검색 (예: 왕, 할리)",
     difficulty="새로운 난이도 선택",
-    description="(예: 앉콩맵, 벽캔맵)",
     creator="새로운 제작자",
+    cp="새로운 단계 (예: 100)",
+    description="(예: 앉콩맵, 벽캔맵)",
+    guide="가이드 링크"
 )
 @app_commands.autocomplete(code=utils.code_autocomplete)
 @app_commands.autocomplete(map_name=utils.map_autocomplete)
@@ -29,27 +33,33 @@ async def update_code_command(
     code: str,
     map_name: str = None,
     difficulty: str = None,
-    description: str = None,
     creator: str = None,
+    cp: int = None,
+    description: str = None,
+    guide: str = None
 ):
     await interaction.response.defer(ephemeral=True)
 
     updates = {
         k: v for k, v in {
-            "map_name":   map_name,
-            "difficulty": difficulty,
+            "map_name":    map_name,
+            "difficulty":  difficulty,
+            "creator":     creator,
+            "cp":          cp,
             "description": description,
-            "creator":    creator,
+            "guide":       guide
         }.items() if v is not None
     }
     code_service.update_code(code=code, updates=updates)
     code_autocomplete_service.load_memory()
 
     lines = [f"> 코드 : **{code}**"]
-    if map_name:   lines.append(f"> 맵 : {map_name}")
-    if difficulty: lines.append(f"> 난이도 : {difficulty}")
+    if map_name:    lines.append(f"> 맵 : {map_name}")
+    if difficulty:  lines.append(f"> 난이도 : {difficulty}")
     if description: lines.append(f"> 설명 : {description}")
-    if creator:    lines.append(f"> 제작자 : {creator}")
+    if cp:          lines.append(f"> 단계 : {cp}")
+    if creator:     lines.append(f"> 제작자 : {creator}")
+    if guide:       lines.append(f"> 가이드 : {guide}")
 
     embed = discord.Embed(
         title="🔨 코드 수정 성공",
